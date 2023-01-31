5a1bae199f63349eca177639e528d5644fe024ee
Nicodev11 <nicolasgardien.developpeur@gmail.com>
2023-01-31 10:39:28 +0100

    Style et fonction Component Navigation terminé

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './assets/bootstrap/bootstrap.min.css'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 import Navigation from './components/Navigation';
 
 const App = () => {
   return (
     <div>
-      <Navigation />
+        <Navigation/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
     </div>
   );
 };
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components/macro";
 
 const Navigation = () => {
@@ -25,19 +26,28 @@ const Navigation = () => {
               <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">
+                    <NavLink
+                      to="/"
+                      className={(nav) => (nav.isActive ? "nav-active" : "")}
+                    >
                       Accueil
-                    </a>
+                    </NavLink>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">
-                    À propos de moi
-                    </a>
+                    <NavLink
+                      to="/about"
+                      className={(nav) => (nav.isActive ? "nav-active" : "")}
+                    >
+                      A propos de moi
+                    </NavLink>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">
+                    <NavLink
+                      to="/contact"
+                      className={(nav) => (nav.isActive ? "nav-active" : "")}
+                    >
                       Contact
-                    </a>
+                    </NavLink>
                   </li>
                 </ul>
               </div>
@@ -68,17 +78,45 @@ const Logo = styled.div`
 `;
 
 const Menu = styled.div`
-.nav-item {
-  font-size: 25px;
-  a {
-    color: white;
-    margin: auto 10px;
+  .nav-item {
+    font-size: 20px;
+
+    a {
+      color: white;
+      margin: auto 10px;
+      text-decoration: none;
+    }
+    .active {
+      border-bottom: solid 2px white;
+    }
+    .nav-active {
+      position: relative;
+
+      &::after {
+        content: "";
+        height: 4px;
+        width: 0;
+        background: red;
+        position: absolute;
+        border-radius: 10px;
+        bottom: -10px;
+        left: 50%;
+        transform: translateX(-50%);
+        -webkit-animation: anim 0.3s ease forwards;
+        animation: anim 0.6s ease forwards;
+
+        @-webkit-keyframes anim {
+          to {
+            width: 100%;
+          }
+        }
+
+        @keyframes anim {
+          to {
+            width: 100%;
+          }
+        }
+      }
+    }
   }
-  .active {
-    color: red;
-    border-bottom: solid 3px red;
-    
-  }
-  
-}
 `;
